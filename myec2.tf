4a514aab1b1688b3713af3fc2cf12bf228a6577b
kakumanu17 <45358447+kakumanu17@users.noreply.github.com>
2022-07-26 12:58:59 +0530

    Update myec2.tf

--- myec2.tf
+++ myec2.tf
@@ -1,3 +1,31 @@
-module "ec2module" {
-  instance_type = "t2.small"
+variable "sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8020, 8080, 8100, 9898, 9200]
+}
+
+resource "aws_security_group" "dynamicsg" {
+  name       = "dynamic-sg"
+  description = "this is a testing dynamic sg"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port  = port.value
+      to_port    = port.value
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "egress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port  = port.value
+      to_port    = port.value
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
